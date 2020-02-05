a9ec69a18769f48e6475d6c5214d5d011b874edb
denni.kurniawan <denni.kurniawan@jatis.com>
2020-02-05 14:54:39 +0700

    a

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ console.log('baw')
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
   console.log('mew')
   res.sendFile(path.join(__dirname+'/dist/angular-material-login-template/index.html'));
 });
