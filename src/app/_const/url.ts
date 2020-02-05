476667b2a64d57d5df3dcceb1cde84b630f69327
denni.kurniawan <denni.kurniawan@jatis.com>
2020-02-05 13:00:17 +0700

    fisrst-commit

---
+++ src/app/_const/url.ts
@@ -0,0 +1,1 @@
+export const SERVER_ADDR = 'https://back-end-denidwik.herokuapp.com';
---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+/* Routing */
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+/* Angular Material */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/* FormsModule */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+/* Angular Flex Layout */
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+/* Components */
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component';
+import {MatCardModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {ClrLoadingModule} from '@clr/angular';
+import {AlertComponent} from './components/alert.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogInComponent,
+    RegisterComponent,
+    RegisterComponent,
+    LogInComponent,
+    AlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    HttpClientModule,
+    ClrLoadingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+
+export class AppModule { }
