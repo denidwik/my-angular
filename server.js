e5fefcadb724289dfe2734f878ceb425ff17bb94
denni.kurniawan <denni.kurniawan@jatis.com>
2020-02-05 14:31:23 +0700

    a

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-material-login-template'));
+console.log('baw')
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
+  console.log('mew')
   res.sendFile(path.join(__dirname+'/dist/angular-material-login-template/index.html'));
 });
 
